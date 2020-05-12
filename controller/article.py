6177b2eb7ed1bcbf93ef4d58a6ed392180cd462f
Outsrkem <xhdascnf@126.com>
2020-05-12 22:26:49 +0800

    文章阅读界面优化，使用积分阅读完成。v4.9

--- controller/article.py
+++ controller/article.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, abort, render_template
+from flask import Blueprint, abort, render_template, request
 from module.article import Article
+from module.credit import Credit
+from module.users import Users
 
 article = Blueprint('article', __name__)
 
 
-
+# 阅读文章
 @article.route('/type/article/<int:articleid>')
 @article.route('/article/<int:articleid>')
 def read(articleid):
@@ -18,14 +20,39 @@ def read(articleid):
     for k, v in result[0].__dict__.items():
         if not k.startswith('_sa_instance_state'):
             dict[k] = v
-    print(dict)
+    # print(dict)
     dict['nickname'] = result.nickname
 
-    # 显示一半文章
-    content = dict['content']
-    temp = content[0:int(len(content)/4)]
-    position = temp.rindex('</p>') + 4
-    dict[content]=temp[0:position]
+    # ，如果没有消耗过积分显示一半文章
+    payed = Credit().check_payed_article(articleid)
+    position = 0
+    if not payed:
+        content = dict['content']
+        # print(content)
+        temp = content[0:int(len(content)/2)]
+        # print(temp)
+        position = temp.rindex('</p>') + 4 #获取文章</p>标签的位置
+        dict['content'] = temp[0:position]
+        # print(dict['content'])
+    Article().update_read_count(articleid) # 阅读次数加1
+    return render_template('article-user.html', article=dict, position=position, payed=payed)
 
-    Article().update_read_count(articleid)
-    return render_template('article-user.html', article=dict)
+# 消耗积分的文章展示文章剩余内容，虽然这个接口不显示，但是还要处理，已防绕开前端界面被直接调用
+@article.route('/article/readall', methods=['POST'])
+def read_all():
+    position = int(request.form.get('position'))
+    articleid = request.form.get('articleid')
+    article = Article()
+    result = article.find_by_id(articleid)
+    content = result[0].content[position:] #获取文章</p>标签的位置后面胡内容
+
+    # 如果已经消耗积分，则不再扣除
+    payed = Credit().check_payed_article(articleid)   #查询是否已经消耗积分
+    if not payed:
+        # 添加积分明细
+        Credit().inster_detail(type='阅读文章',target = articleid, credit=-1*result[0].credit)
+        # 减少用户表胡剩余积分
+        Users().update_credit(credit=-1*result[0].credit)
+    return content
+
+
--- module/credit.py
+++ module/credit.py
@@ -2,16 +2,30 @@ from flask import session
 from sqlalchemy import Table
 from common.database import dbconnect
 import time
-dbsession, md ,DBase = dbconnect()
+
+dbsession, md, DBase = dbconnect()
+
+
 class Credit(DBase):
     __table__ = Table('credit', md, autoload=True)
 
-    def inster_detail(self,type,target,credit):
+    def inster_detail(self, type, target, credit):
         now = time.strftime('%Y-%m-%d %H:%M:%S')
-        credit = Credit(userid=session.get('userid'), category=type, target=target, credit=credit, createtime=now, updatetime=now)
+        credit = Credit(userid=session.get('userid'), category=type, target=target, credit=credit, createtime=now,
+                        updatetime=now)
         print(session.get('userid'))
         dbsession.add(credit)
         dbsession.commit()
 
-if __name__ == '__main__':
-    Credit().inster_detail(type='用户注册', target='0', credit=50)
+    # 判断用户是否已经消耗积分
+    def check_payed_article(self, articleid):
+        result = dbsession.query(Credit).filter_by(userid=session.get('userid'), target=articleid).all() #返回的是一个列表
+        print(result)
+        if len(result) > 0:
+            return True
+        else:
+            return False
+
+
+# if __name__ == '__main__':
+#     Credit().inster_detail(type='用户注册', target='0', credit=50)
